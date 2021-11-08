32f57d65c0202ec1b5cd375cca4c075f0d716bf8
Barbara_Ferro <barbaracaetanoferro@gmail.com>
2021-11-08 19:07:35 -0300

    exercicios bonus 1 2

---
+++ fundamentos/bloco-4-fundamentos-javascript/dia-2-modulo-4/bonus-exercicio-2.js
@@ -0,0 +1,16 @@
+// Ordene o array numbers em ordem decrescente e imprima seus valores;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+console.log(numbers);
+
+for (let index = 1; index < numbers.length; index += 1){
+    for (let i = 0; i < index; i +=1) {
+        if (numbers[index] > numbers[i]){
+            let ordem = numbers[index];
+            numbers[index] = numbers[i];
+            numbers[i] = ordem;
+        }
+    }
+}
+
+console.log(numbers);
