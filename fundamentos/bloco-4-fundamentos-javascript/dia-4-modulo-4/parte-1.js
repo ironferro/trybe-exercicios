97cc9a1369d606621ffc6b4bf9d907c581464950
Barbara_Ferro <barbaracaetanoferro@gmail.com>
2021-11-10 18:44:49 -0300

    exercicios do 6 ao 8

--- fundamentos/bloco-4-fundamentos-javascript/dia-4-modulo-4/parte-1.js
+++ fundamentos/bloco-4-fundamentos-javascript/dia-4-modulo-4/parte-1.js
@@ -1,11 +1,11 @@
 //OBJETOS E FOR/IN
 
-let info = {
+/*let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
     recorrente: 'Sim'
-  };
+  };*/
 
 //1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. 
 
@@ -28,7 +28,7 @@ let info = {
 
 //5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
 
-let info2 = {
+/*let info2 = {
     personagem: 'Tio Patinhas',
     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
     nota: 'O último MacPatinhas',
@@ -41,4 +41,35 @@ for (let index in info) {
     } else {
         console.log(info[index] + ' e ' + info2[index]);
     }
-}
+}*/
+
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+//6 - Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+/*console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo);*/
+
+//7 - Adicione um novo livro favorito na chave livrosFavoritos , que é um array .
+leitor.livrosFavoritos.push(
+    {
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'JK Rowling',
+        editor: 'Rocco',
+    }
+)
+//console.log(leitor);
+
+//8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
+
+console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos');
